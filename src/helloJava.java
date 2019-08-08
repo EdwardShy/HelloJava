fffd8f74f1894fa5b1ecca786677a9662cf0fdbb
EdwardShy <1215842712@qq.com>
2019-08-08 12:37:55 +0800

    第一次修改，添加了时间

--- src/helloJava.java
+++ src/helloJava.java
@@ -1,8 +1,9 @@
-
+import java.util.Date;
 public class helloJava {
 	public static void main(String[] args) {
-
+		Date now = new Date();
 
 		System.out.println("Hello JAVA abc ");
+		System.out.println("第一次修改:"+now);
 	}
 }
